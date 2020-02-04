8da0932e5a2880b4fca03d20671c01f173bfb344
mk668a <msftoaa@gmail.com>
2020-02-04 22:42:48 +0900

    css modulesの作成

---
+++ src/components/container.js
@@ -0,0 +1,8 @@
+import React from "react"
+import containerStyles from "./container.module.css"
+
+console.log(containerStyles)
+
+export default ({ children }) => (
+    <div className={containerStyles.container}>{children}</div>
+)
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,5 +2,13 @@ import React from "react"
 import { Link } from "gatsby"
 
 export default () => (
-    <Link to="/about">About</Link>
+    <div>
+        <h1> Top Page </h1>
+        <div className="flex-column">
+            <Link to='/about'>About</Link>
+            <Link to='/about-css-modules'>About CSS MODULES</Link>
+            <Link to='/contact'>contact</Link>
+            <Link to='/container'>container</Link>
+        </div>
+    </div>
 )
