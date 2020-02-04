456388688800d14d6cbcef37861c429b5ffb1234
mk668a <msftoaa@gmail.com>
2020-02-04 21:51:44 +0900

    pageの追加/Link to

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { Link } from "gatsby"
+import Header from "../components/header"
+
+export default () => (
+    <div style={{ color: `teal` }}>
+        <Link to="/">Home</Link>
+        <Link to="/contact">Contact</Link>
+        <Header headerText="About Gatsby" />
+        <Header headerText="It's pretty cool" />
+        <h1>About Gatsby</h1>
+        <p>Such wow. Very React.</p>
+    </div>
+)
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,6 @@
 import React from "react"
+import { Link } from "gatsby"
 
-export default () => <div>Hello world!</div>
+export default () => (
+    <Link to="/about">About</Link>
+)
