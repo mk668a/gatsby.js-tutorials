e132c5c125cd9c97870c4c76afdb34f5aada5566
mk668a <msftoaa@gmail.com>
2020-02-21 00:28:42 +0900

    exit tutorial

--- src/pages/mark-down.js
+++ src/pages/mark-down.js
@@ -1,11 +1,10 @@
 import React from "react"
-import { graphql } from "gatsby"
 import { css } from "@emotion/core"
+import { Link, graphql } from "gatsby"
 import { rhythm } from "../utils/typography"
 import Layout from "../components/layout"
 
 export default ({ data }) => {
-    console.log(data)
     return (
         <Layout>
             <div>
@@ -20,22 +19,29 @@ export default ({ data }) => {
                 <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
                 {data.allMarkdownRemark.edges.map(({ node }) => (
                     <div key={node.id}>
-                        <h3
+                        <Link
+                            to={node.fields.slug}
                             css={css`
-                margin-bottom: ${rhythm(1 / 4)};
+                text-decoration: none;
+                color: inherit;
               `}
                         >
-                            {node.frontmatter.title}{" "}
-                            <span
+                            <h3
                                 css={css`
-                  color: #bbb;
+                  margin-bottom: ${rhythm(1 / 4)};
                 `}
                             >
-                                — {node.frontmatter.date}
-                            </span>
-                        </h3>
-                        <p>{node.excerpt}</p>
-                        {/* <div>{node.html}</div> */}
+                                {node.frontmatter.title}{" "}
+                                <span
+                                    css={css`
+                    color: #555;
+                  `}
+                                >
+                                    — {node.frontmatter.date}
+                                </span>
+                            </h3>
+                            <p>{node.excerpt}</p>
+                        </Link>
                     </div>
                 ))}
             </div>
@@ -45,7 +51,7 @@ export default ({ data }) => {
 
 export const query = graphql`
   query {
-    allMarkdownRemark {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
       totalCount
       edges {
         node {
@@ -54,8 +60,10 @@ export const query = graphql`
             title
             date(formatString: "DD MMMM, YYYY")
           }
+          fields {
+            slug
+          }
           excerpt
-          html
         }
       }
     }
